b55c3e39cd36fb8862e647f486f3ddaefe0fcdc7
Hasan OKTEN <hhoohh2001@yahoo.com>
2013-05-03 12:38:42 +0000

    Ejs and MongoDB version

--- app.js
+++ app.js
@@ -2,17 +2,7 @@
  * Module dependencies
  */
 var async = require('async');
-var express = require('express')
-	, stylus = require('stylus')
-	, nib = require('nib');
-var mysql      = require('mysql');
-var mongoose = require('mongoose');
-var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : '66666666',
-	database : 'chart'
-});
+var express = require('express'), stylus = require('stylus'), nib = require('nib');
 var moment = require('moment');
 var app = express();
 
@@ -22,26 +12,11 @@ function compile(str, path) {
 	.use(nib());
 }
 
-// Mongodb model schemes
-mongoose.connect('mongodb://localhost/test');
-var Schema = mongoose.Schema; 
-var user = new Schema({
-	user_id : String,
-	event_id : String,
-	gender : String,
-	birthday : Date, 
-	date_created : Date
-});
+var Db = require('mongodb').Db,
+Connection = require('mongodb').Connection,
+Server = require('mongodb').Server;
 
-var event = new Schema({
-	event_id : String,
-	event_title : String,
-	event_start : Date,
-	event_end : Date, 
-	date_created : Date
-});
-var user = mongoose.model('user', user);
-var event = mongoose.model('event', event);
+
 
 function dateArraytoString(dateArray) {
 	var dateString="";
@@ -72,9 +47,6 @@ function dateArraytoString(dateArray) {
 
 
 
-
-
-
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 app.use(express.logger('dev'))
@@ -87,6 +59,7 @@ app.use(express.static(__dirname + '/public'))
 
 
 app.get('/logget', function(req, res){
+	// Dates range from select box.
 	var dates = req.param('tarih').split('_');
 	var visitdate = {
 		vdates:[],
@@ -115,159 +88,161 @@ app.get('/logget', function(req, res){
 	var kadin=0;
 
 
-
-
 	async.parallel(
 		{
-		visits: function(callback){
+
+
+		// For line chart datas
+		// For each day, calculating visits count
+		visits: function(callback)
+		{
 			for(var i=0; i<dates.length; i++){
 				(function(s){
-					event.count({'event_start': {
-						'$gte':moment(dates[s],'DD-MM-YYYY'),
-						'$lt':moment(dates[s],'DD-MM-YYYY').add('days',1)
-					}},
-					function(err, c){
-						visitdate.vdates[s]=dates[s]+'.'+c;
-						visitdate.vcounts[s]=c;
-						if(k==dates.length-1) {
-							callback(null, visitdate);
-						}
-						k++;
+					var db = new Db('test', new Server("localhost",27017, {}, {w: 1}), {safe:true});
+					db.open(function(err, db) {
+						db.collection('events', function(err, collection) {
+							collection.find({'event_start': {$gte:moment(dates[s],'DD-MM-YYYY').toDate(),$lt:moment(dates[s],'DD-MM-YYYY').add('days',1).toDate()	}},function(err, cursor){
+								cursor.count(function(err, count){
+									visitdate.vdates[s]=dates[s]+'.'+count;
+									visitdate.vcounts[s]=count;
+									if(k==dates.length-1) {
+										db.close();
+										callback(null, visitdate);
+									}
+									k++;
+								});
+							});
+						});
 					});
 				}(i)); // <-- this is the parameter of the closure
 			}
 
 		},
-		distbs: function(callback){
+		distbs:	function(callback)	{
 			async.waterfall(
-				[function(callback) {
-
-
-				event.find({'event_start': {
-					'$gte':moment(dates[0],'DD-MM-YYYY'),
-					'$lt':moment(dates[dates.length-1],'DD-MM-YYYY').add('days',1)
-				}},'event_id', function (err, docs) {
-					callback(null, docs);
-				});
-			},
-			function(arg1, callback){
-				for(var m=0; m<arg1.length; m++) {
-					(function(t){
-
-						user.find({'event_id': arg1[t].event_id },function (err, docs) {
-							console.log(docs[0].birthday)
-							var visitor_age = moment().diff(moment(docs[0].birthday),'years');
-
-							if(visitor_age<=10) {
-								visitors.ages.ten++;
-							}
-							else if(visitor_age <= 20) {
-								visitors.ages.twenty++;
-							}
-							else if(visitor_age <= 30) {
-								visitors.ages.thirty++
-							}
-							else if(visitor_age <= 40) {
-								visitors.ages.forty++;
-							}
-							else if(visitor_age <= 50) {
-								visitors.ages.fifty++;
-							}
-							else {
-								visitors.ages.other++;
-							}
-
-
-							
-							if(docs[0].gender=="male") {
-								visitors.genders.male++;
-							}
-							else {
-								visitors.genders.female++;
-							}
-
-
-							if(a==arg1.length-1) {
-								callback(null,visitors);
-							}
-							a++;
-
+				[
+					/* ###############################
+					waterfall first function
+					#################################*/
+					function(callback)
+					{
+						// Selecting all records from "events" collection where  beetween start date and end date
+						var db = new Db('test', new Server("localhost",27017, {}, {w: 1}), {safe:true});
+					   	db.open(function(err, db) {
+							db.collection('events', function(err, collection) {
+								collection.find({'event_start': {
+									$gte:moment(dates[0],'DD-MM-YYYY').toDate(),$lt:moment(dates[dates.length-1],'DD-MM-YYYY').add('days',1).toDate() }}).toArray(function(err, result) 
+									{
+										// "result" pass paramater "arg1" of next function.
+										db.close();
+										callback(null,result);
+									});
+							});
 						});
-					}(m)); // <-- this is the parameter of the closure
-
-
-
-
-
-				}
-
-			}
-			], function (err, result) {
-				callback(null,result);
-
-			});
-		},
-	},
-	function(e, r){
-	     res.header('Content-Type', 'application/json');
-			     res.header('Charset', 'utf-8')  
-			     res.send(JSON.stringify(r));  
-
-
+					},
+					// second waterfall first function
+					function(arg1, callback)
+					{
+						for(var m=0; m<arg1.length; m++)
+						{
+							(function(t) {
+								var db = new Db('test', new Server("localhost",27017, {}, {w: 1}), {safe:true});
+								db.open(function(err, db) {
+									db.collection('users', function(err, collection) {
+										collection.find({'event_id': arg1[t].event_id}).toArray(function (err, result) {
+											var visitor_age = moment().diff(moment(result[0].birthday),'years');
+											if(visitor_age<=10) {
+												visitors.ages.ten++;
+											}
+											else if(visitor_age <= 20) {
+												visitors.ages.twenty++;
+											}
+											else if(visitor_age <= 30) {
+												visitors.ages.thirty++;
+											}
+											else if(visitor_age <= 40) {
+												visitors.ages.forty++;
+											}
+											else if(visitor_age <= 50) {
+												visitors.ages.fifty++;
+											}
+											else {
+												visitors.ages.other++;
+											}
+											if(result[0].gender=="male") {
+												visitors.genders.male++;
+											}
+											else {
+												visitors.genders.female++;
+											}
+											if(a==arg1.length-1) {
+												callback(null,visitors);
+											}
+											a++;
+										});
+									});
+								});
+							}(m)); // <-- this is the parameter of the closure 
+						}
+					}],
+					function (err, result) {
+						callback(null,result);
+					});
+		}
+	}, function(e, r){
+		res.header('Content-Type', 'application/json');
+		res.header('Charset', 'utf-8')  
+		res.send(JSON.stringify(r));  
 	});
-
-
-
-
-
-
-
-
-
 });
 
 
+// Route firstly open page. 
 app.get('/', function (req, res) {
 	var dates=[];
 	var test=true;
 	var s=0;
 	var currentDate;
 	var rows=[];
-	// Asenkron sorunu yuzunden async kullanildi
-	event.find({},'event_start').sort('event_start').exec(function(error, data){
-		async.eachSeries(data, function (prime, callback) {
-			rows.push(moment(prime.event_start).format('DD-MM-YYYY'));
-			callback(); // Alternatively: callback(new Error());
-		}, function (err) {
-			if (err) { throw err; }
-		});
 
 
-		// Bu bolumde diziye atilan tarihler haftlara
-		// bolunecek sekilde cok boyutlu dizi haline getiriliyor
+	var db = new Db('test', new Server("localhost",27017, {}, {w: 1}), {safe:true});
+	db.open(function(err, db) {
+		db.collection('events', function(err, collection) {
+			collection.find({},{'sort':'event_start'}).toArray(function (err, result) {
+				async.eachSeries(result, function (prime, callback) {
+					rows.push(moment(prime.event_start).format('DD-MM-YYYY'));
+					callback(); // Alternatively: callback(new Error());
+				}, function (err) {
+					if (err) { throw err; }
+				});
 
-		var startDate = moment(rows[0],'DD-MM-YYYY');
-		var week=[startDate.format('DD-MM-YYYY')];
-		var endDate = moment(rows[rows.length-1], "DD-MM-YYYY");
+				// Bu bolumde diziye atilan tarihler haftlara
+				// bolunecek sekilde cok boyutlu dizi haline getiriliyor
 
-		var h=0;
-		var week=[];
-		while(test) {
-			for(s=0; s<12; s++) {
-				if(endDate.isBefore(startDate)) {
-					test=false;
-					break;
+				var startDate = moment(rows[0],'DD-MM-YYYY');
+				var week=[startDate.format('DD-MM-YYYY')];
+				var endDate = moment(rows[rows.length-1], "DD-MM-YYYY");
+
+				var h=0;
+				var week=[];
+				while(test) {
+					for(s=0; s<12; s++) {
+						if(endDate.isBefore(startDate)) {
+							test=false;
+							break;
+						}
+						week.push(startDate.format('DD-MM-YYYY'));
+						startDate.add('days',1);
+					}
+					dates.push(week);
+					week=[];
 				}
-				week.push(startDate.format('DD-MM-YYYY'));
-				startDate.add('days',1);
-			}
-			dates.push(week);
-			week=[];
-		}
-		console.log("Son denem");
-		console.log(dates);
-		res.render('index', {title : dateArraytoString(dates)});
+				db.close();
+				res.render('index', {title : dateArraytoString(dates)});
+			});
+		});
 	});
-})
+});
 
 app.listen(2222)
